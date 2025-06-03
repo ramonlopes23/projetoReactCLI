620809c45d8b0830b6602dae1ae15806a794f0b3
Ramon <ramon.lopes2014@hotmail.com>
2025-06-03 19:52:49 -0300

    startcommit

---
+++ my-app/src/pages/home/index.jsx
@@ -0,0 +1,7 @@
+const Home = () =>{<>
+    return (
+        <h1>Home</h1>
+    <button>Fazer login</button>)
+</>}
+
+export {Home}
---
+++ my-app/src/pages/login/index.jsx
@@ -0,0 +1,7 @@
+const Login = () =>{<>
+    return (
+        <h1>Login</h1>
+    <button>Voltar para a home</button>)
+</>}
+
+export {Login}
